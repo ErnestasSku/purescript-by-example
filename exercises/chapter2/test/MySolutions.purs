a38832d4472f903d0418186b14b6f934a7ec6e5f
ErnestasSku <ernestassku@gmail.com>
2022-04-21 20:01:38 +0300

    Solve chapter 2

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,12 @@
 module Test.MySolutions where
 
 import Prelude
+import Math (sqrt, pi)
+import Data.Int (rem)
+import Data.Function (flip)
+
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea r = pi * r * r
+
+leftoverCents = flip rem 100
